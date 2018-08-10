fb843a77d7ede9a491b13f90240f31898dde0648
sogoiii <angellopozo@gmail.com>
2018-08-10 00:38:32 -0700

    updated webpack and package.json to make this work with updated truffle-solidity-loader.

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const path = require('path')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+
+module.exports = {
+  entry: './app/scripts/index.js',
+  mode: 'production',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'app.js'
+  },
+  plugins: [
+    // Copy our app's index.html to the build folder.
+    new CopyWebpackPlugin([
+      { from: './app/index.html', to: 'index.html' }
+    ])
+  ],
+  devtool: 'source-map',
+  module: {
+    rules: [
+      { test: /\.s?css$/, use: [ 'style-loader', 'css-loader', 'sass-loader' ] },
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['env'],
+          plugins: ['transform-react-jsx', 'transform-object-rest-spread', 'transform-runtime']
+        }
+      },
+      {
+        test: /\.sol/,
+        use: [
+          {
+            loader: 'json-loader'
+          },
+          {
+            loader: 'truffle-solidity-loader',
+            options: {
+              migrations_directory: path.resolve(__dirname, './migrations'),
+              network: 'ganache',
+              contracts_build_directory: path.resolve(__dirname, './build/contracts')
+            }
+          }
+        ]
+      }
+    ]
+  }
+}
+
